785b7b438d343cebddbabb1d59251f5435dc0f04
shoorano <56363369+shoorano@users.noreply.github.com>
2020-07-16 21:26:30 +0100

    Applied firebase utils to sign up component and added to app.js with styling.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx';
 import Header from './components/header/header.jsx';
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
 class App extends Component {
   constructor() {
@@ -21,8 +21,21 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     })
   }
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import SignIn from '../../components/sign-in/sign-in.jsx'
+import SignIn from '../../components/sign-in/sign-in.jsx';
+import SignUp from '../../components/sign-up/sign-up.jsx';
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 )
 
