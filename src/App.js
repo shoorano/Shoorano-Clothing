ca9f65b5c060506fd690fa3b7b5075c36a785db3
shoorano <56363369+shoorano@users.noreply.github.com>
2020-07-16 16:16:43 +0100

    Added logo svg and header component with styling.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.jsx'
 import ShopPage from './pages/shop/shop.jsx'
+import Header from './components/header/header.jsx'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={ HomePage } />
         <Route path='/shop' component={ ShopPage } />
